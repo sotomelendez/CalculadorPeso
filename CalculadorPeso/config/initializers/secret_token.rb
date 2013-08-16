426cfb1d6042cfbb82221cfcdd5b373b07e20768
Sebastian Gil Parga <s.gilparga@gmail.com>
2013-08-16 18:56:24 -0500

    Creacion del proyecto con Rails

---
+++ CalculadorPeso/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalculadorPeso::Application.config.secret_key_base = '834bae468c5bd16de5a4f7034a5af10499681aca78d74285c3585dd4321f1cec049b2cbe2158fe303a9c018450436890c60d2fe14448e2713144bdf29c4d7d26'
